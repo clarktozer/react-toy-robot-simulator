94a30c06eca266c4ed2ea69542c43b63bde4a794
Clark Tozer <clark.tozer@sope.com.au>
2020-04-22 10:50:40 +1000

    Updates

---
+++ precache-manifest.489e7c38cce056df72ac414c34181c60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa9d40936ee20d8daad9bea5399015a",
+    "url": "/react-toy-robot-simulator/index.html"
+  },
+  {
+    "revision": "dd4c2ac60374bc14ad41",
+    "url": "/react-toy-robot-simulator/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "8cd53e81ea6aca26581f",
+    "url": "/react-toy-robot-simulator/static/css/main.797696d6.chunk.css"
+  },
+  {
+    "revision": "dd4c2ac60374bc14ad41",
+    "url": "/react-toy-robot-simulator/static/js/2.c4f69845.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-toy-robot-simulator/static/js/2.c4f69845.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cd53e81ea6aca26581f",
+    "url": "/react-toy-robot-simulator/static/js/main.9bbfaa1c.chunk.js"
+  },
+  {
+    "revision": "28d72a13e9291279aea4",
+    "url": "/react-toy-robot-simulator/static/js/runtime-main.73c9f1f3.js"
+  }
+]);
